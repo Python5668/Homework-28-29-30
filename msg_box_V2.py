c5e40c10c19efbb95b0fe6b74cd17c78d5e1343f
Python5668 <48457709+Python5668@users.noreply.github.com>
2019-04-13 12:38:29 -0600

    Add files via upload
    This is the second version with a button and a slider.

--- msg_box_V2.py
+++ msg_box_V2.py
@@ -7,6 +7,25 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+'''user code'''
+import RPi.GPIO as GPIO
+from gpiozero import LED
+
+GPIO.setmode(GPIO.BCM)
+led = LED(23) # for the LED toggling project
+
+ledPin = 24 # for the LED dimmer project
+GPIO.setup(ledPin, GPIO.OUT)
+pwm = GPIO.PWM(ledPin, 100)
+pwm.start(100)
+
+def toggleLED():
+    if led.is_lit:
+        led.off()
+    else:
+        led.on()
+'''user code'''
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -23,7 +42,15 @@ class Ui_MainWindow(object):
         font.setWeight(75)
         self.pushButton.setFont(font)
         self.pushButton.setObjectName("pushButton")
+        
         self.verticalSlider = QtWidgets.QSlider(self.centralwidget)
+        
+        '''user code'''
+        self.verticalSlider.setMinimum(0)
+        self.verticalSlider.setMaximum(100)
+        self.verticalSlider.setValue(100)
+        '''user code'''
+        
         self.verticalSlider.setGeometry(QtCore.QRect(370, 70, 101, 291))
         self.verticalSlider.setOrientation(QtCore.Qt.Vertical)
         self.verticalSlider.setObjectName("verticalSlider")
@@ -39,4 +66,22 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Message Box"))
         self.pushButton.setText(_translate("MainWindow", "Click Me"))
+        '''user code'''
+        self.pushButton.clicked.connect(toggleLED)
+        self.verticalSlider.valueChanged.connect(self.sliderMove)
+        
+    def sliderMove(self):
+        value = self.verticalSlider.value()
+        print(value)
+        pwm.ChangeDutyCycle(value)
+        '''user code'''
 
+'''user code'''
+import sys
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
+'''user code'''
