d28a895e83bcf4e1a9e53151b12659ad9b2ec728
Python5668 <48457709+Python5668@users.noreply.github.com>
2019-04-15 19:35:07 -0600

    Add files via upload

--- HW30_Step1_and_2.py
+++ HW30_Step1_and_2.py
@@ -1,33 +1,33 @@
-# Task 1, arduino serial. This is the simplest way to collect serial data using Arduino + Windows
-import serial
-import matplotlib.pyplot as plt
+### Task 1, arduino serial. This is the simplest way to collect serial data using Arduino + Windows
+#import serial
+#import matplotlib.pyplot as plt
 
-ser = serial.Serial('COM3', 9600)
-n = 0
-dataLst = []
-while n<200: # collect 200 data points and stop
-    print(ser.readline())
-    dataPoint = ser.readline()
-    dataPoint = int(dataPoint) # the data received are strings
-    dataLst.append(dataPoint)
-    n+=1
-#print(type(dataPoints))
-#print(dataPoints)
-ser.close() # Dont forget to close the serial port
-    
-plt.plot(dataLst) # plot the received data afterwards
-#plt.show() # if you run this code in Spyder, you dont need this line
-f = open("serialData.dat", "w") # saves the data to a file
-f.write(str(dataLst)) # string is the only legal format to be written into a file
-f.close()
-f = open('serialData.dat', 'r')
-print(f.read())
+#ser = serial.Serial('COM6', 9600) # Gatta change this for every new COM
+#n = 0
+#dataLst = []
+#while n<200: # collect 200 data points and stop
+#    print(ser.readline())
+#    dataPoint = ser.readline()
+#    dataPoint = int(dataPoint) # the data received are strings
+#    dataLst.append(dataPoint)
+#    n+=1
+##print(type(dataPoints))
+##print(dataPoints)
+#ser.close() # Dont forget to close the serial port
+#    
+#plt.plot(dataLst) # plot the received data afterwards
+##plt.show() # if you run this code in Spyder, you dont need this line
+#f = open("serialData.dat", "w") # saves the data to a file
+#f.write(str(dataLst)) # string is the only legal format to be written into a file
+#f.close()
+#f = open('serialData.dat', 'r')
+#print(f.read())
 
-# Task 2, use pyqtgraph to plot the data instead of using matplotlib
+### Task 2, use pyqtgraph to plot the data instead of using matplotlib
 #import pyqtgraph as pq
 #from pyqtgraph.Qt import QtCore, QtGui
 #from pyqtgraph import PlotWidget
-#import numpy as np
+
 #
 ### Always start by initializing Qt (only once per application)
 #app = QtGui.QApplication([])
@@ -62,3 +62,73 @@ print(f.read())
 #w.show()
 ### start the Qt event loop
 #app.exec_()
+
+### Task 3, live plot the serial data the with pyqtgraph
+from PyQt5 import QtCore, QtGui, QtWidgets
+from pyqtgraph import PlotWidget
+import serial
+import sys
+import numpy as np
+import pyqtgraph as pg
+
+ser = serial.Serial('COM6', 9600)
+
+class ExampleApp(QtGui.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        pg.setConfigOption('background', 'w')
+        self.setupUi(self)
+        
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(900,900)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        # the plot widget
+        self.graphicsView = PlotWidget(self.centralwidget) # assign this PlotWidget to the graphicsView
+        self.graphicsView.setGeometry(QtCore.QRect(200, 200, 500, 500))
+        self.graphicsView.setObjectName("graphicsView")
+        #
+        MainWindow.setCentralWidget(self.centralwidget)
+        
+    def update(self):
+        points = 100 # number of data points
+        X = np.arange(points)
+        n = 0
+        dataLst = []
+        while n < 100:
+            dataPoint = ser.readline()
+            dataPoint = int(dataPoint)
+            dataLst.append(dataPoint)
+            n += 1
+        Y = dataLst
+        penn = pg.mkPen('k', width = 3, style = QtCore.Qt.SolidLine)
+        self.graphicsView.setYRange(0, 1200, padding = 0)
+        labelStyle = {'color':'#000', 'font-size':'20px' }
+        self.graphicsView.setLabel('bottom', 'Number of Points', '', **labelStyle)
+        self.graphicsView.setLabel('left', 'Voltage', '', **labelStyle)
+        self.graphicsView.plot(X, Y, pen = penn, clear = True)
+        QtCore.QTimer.singleShot(1, self.update) # 1 ms, QUICKLY repeat, recursively. Use this timer to invoke the 
+        # 'update()' function recursively
+        
+app = QtGui.QApplication(sys.argv)
+form = ExampleApp()
+form.show()
+form.update() # start with something
+app.exec_()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
